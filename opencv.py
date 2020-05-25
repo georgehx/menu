98e4efdcc16d307d9debfb4722a002bad2ea77ff
jokergeorge <georgehanxiao@msn.com>
2020-05-25 13:14:57 -0400

    Finalize Casc Approach

--- opencv.py
+++ opencv.py
@@ -14,37 +14,35 @@ import cv2
 import sys
 
 imagePath = "pictures/mcd_very_long.jpg"
-cascPath = "haarcascade_pedestrian.xml"
-# cascPath = "pedestrian_another.xml"
-# cascPath = "haarcascade_fullbody.xml"
+cascPath = "haarcascade_customer.xml"
 
-pedsCascade =  cv2.CascadeClassifier(cascPath)
+customerCascade =  cv2.CascadeClassifier(cascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# detect pedestrian in pic
+# detect customer in pic
 
-peds = pedsCascade.detectMultiScale(
+customers = customerCascade.detectMultiScale(
         gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(60, 40)
+        scaleFactor=1.2, # controlls fine (smaller) vs coarse (larger) trade off, needs to > 1.0
+        minNeighbors=5, # used to combine overlapping small boxes into big one
+        minSize=(60, 40) # box size, distance between recoginized customers
 )
 
-print("Found {0} pedestrian!".format(len(peds)))
+print("Found {0} customers!".format(len(customers)))
 
-# Draw a rectangle around the peds
-for (x, y, w, h) in peds:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 1)
+# Draw a rectangle around the customers
+for (x, y, w, h) in customers:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# cv2.imshow("Faces found", image)
-status = cv2.imwrite('peds_saved.jpg', image)
+# cv2.imshow("customers found", image)
+status = cv2.imwrite('customers_saved.jpg', image)
 print ("Image written to file-system : ",status)
 print(imagePath)
 
-
-#cafe_short: 1.2, 7, 40, 40
-#mcd_short: 1.2, 5, 60, 40
-#mcd_very_long: 1.2, 5, 60, 40
+# hyperparameter sets:
+# cafe_short: 1.2, 7, 40, 40
+# mcd_short: 1.2, 5, 60, 40
+# mcd_very_long: 1.2, 5, 60, 40
